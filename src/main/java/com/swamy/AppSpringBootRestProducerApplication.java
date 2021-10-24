dabb00873e0f70cb73610cfd7647ba0b36119df8
swamymasna <swamymasna55@gmail.com>
2021-10-24 19:44:38 +0530

    SBRA01: First Commit

---
+++ src/main/java/com/swamy/AppSpringBootRestProducerApplication.java
@@ -0,0 +1,13 @@
+package com.swamy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBootRestProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringBootRestProducerApplication.class, args);
+	}
+
+}
