7c3b3c49f4b151f1bb3c7f64beb2110f38188ef4
swamymasna <swamymasna55@gmail.com>
2021-10-25 14:51:41 +0530

    first commit

--- src/main/java/com/swamy/AppSpringBootRestProducerApplication.java
+++ src/main/java/com/swamy/AppSpringBootRestProducerApplication.java
@@ -2,12 +2,15 @@ package com.swamy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AppSpringBootRestProducerApplication {
-
+	
 	public static void main(String[] args) {
-		SpringApplication.run(AppSpringBootRestProducerApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(AppSpringBootRestProducerApplication.class, args);
+		System.out.println(context);
 	}
 
 }
+
